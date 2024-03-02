eb8836c9e418b3fb433ffbeb1845b6eb17de8ea5
dimovv23 <ggdimovv@gmail.com>
2024-03-02 15:24:05 +0200

    Added the Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
