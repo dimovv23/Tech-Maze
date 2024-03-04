bc3ed0bfc7ad18ca55d33eb53aabcd4040b88cc6
dimovv23 <ggdimovv@gmail.com>
2024-03-04 16:12:21 +0200

    Added the signup route, middleware and errorHandler function

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "../server/routes/user.route.js";
+import authRoutes from "../server/routes/auth.route.js";
 
 dotenv.config();
 
@@ -17,8 +18,21 @@ mongoose
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
