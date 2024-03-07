d9893784300e019fc4c85042bab9a03eb64a222a
dimovv23 <ggdimovv@gmail.com>
2024-03-07 12:29:35 +0200

    Add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import FooterComp from "./components/FooterComp";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
+      <FooterComp />
     </BrowserRouter>
   );
 };
---
+++ client/src/components/FooterComp.jsx
@@ -0,0 +1,60 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsTwitterX } from "react-icons/bs";
+
+const FooterComp = () => {
+  return (
+    <Footer container className="border-t-8">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              Logo
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:gap-6">
+            <div>
+              <Footer.Title title="Information" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://georgi-dimovv.vercel.app/"
+                  target="_blank"
+                >
+                  About us
+                </Footer.Link>
+                <Footer.Link href="https://github.com/dimovv23" target="_blank">
+                  GitHub
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms & Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Tech World"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6  mt-4 sm:justify-center">
+            <Footer.Icon icon={BsFacebook} />
+            <Footer.Icon icon={BsInstagram} />
+            <Footer.Icon icon={BsTwitterX} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+};
+
+export default FooterComp;
