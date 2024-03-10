c7ca8f8d4b791b84473bc18fb2427bf8a6ffa907
dimovv23 <ggdimovv@gmail.com>
2024-03-10 18:09:19 +0200

    Add 'update user' api route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "../server/routes/user.route.js";
 import authRoutes from "../server/routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -19,9 +20,10 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port  ${PORT}`);
 });
 
 app.use("/api/user", userRoutes);
