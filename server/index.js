dbbd0b39de7147c3d8f68eef158bf9b2dcc4fc3a
dimovv23 <ggdimovv@gmail.com>
2024-03-13 15:16:36 +0200

    Add 'create post' api route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "../server/routes/user.route.js";
 import authRoutes from "../server/routes/auth.route.js";
+import postRoutes from "../server/routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -28,6 +29,7 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
---
+++ server/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postScheme = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://contenthub-static.grammarly.com/blog/wp-content/uploads/2017/11/how-to-write-a-blog-post.jpeg",
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postScheme);
+
+export default Post;
