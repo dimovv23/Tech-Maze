58e93f4e4fe8856f0fd92ec25881f43fd2b9a042
dimovv23 <ggdimovv@gmail.com>
2024-03-29 15:10:52 +0200

    Add like functionality for the comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import moment from "moment";
+import { FaHeart } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, onLike }) => {
   const [user, setUser] = useState({});
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -39,6 +42,25 @@ const Comment = ({ comment }) => {
         <p className=" text-gray-500 dark:text-gray-400 pb-2">
           {comment.content}
         </p>
+        <div className="flex items-center gap-2 pt-2">
+          <button
+            type="button"
+            className={`text-gray-400 hover:text-red-500 ${
+              currentUser &&
+              comment.likes.includes(currentUser._id) &&
+              "!text-red-500"
+            }`}
+            onClick={() => onLike(comment._id)}
+          >
+            <FaHeart className="text-sm" />
+          </button>
+          <p className="text-gray-500 text-sm">
+            {comment.numOfLikes > 0 &&
+              comment.numOfLikes +
+                " " +
+                (comment.numOfLikes === 1 ? "like" : "likes")}
+          </p>
+        </div>
       </div>
     </div>
   );
