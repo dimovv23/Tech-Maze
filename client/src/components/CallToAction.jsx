a258b04b6c752739f5de83c8cc4f7afef89c7e4d
dimovv23 <ggdimovv@gmail.com>
2024-04-21 17:25:20 +0300

    Add CallToAction component for SumzAI

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button } from "flowbite-react";
+
+const CallToAction = () => {
+  return (
+    <div className="flex flex-col md:flex-row mb-6 gap-8">
+      <div className="flex flex-col justify-center gap-2 2xl:gap-4 text-center md:text-start">
+        <h2 className="font-semibold text-2xl sm:text-4xl 2xl:text-5xl text-white">
+          Overwhelmed by Long Reads?
+        </h2>
+        <p className="text-sm sm:text-lg 2xl:text-2xl text-stone-400">
+          Use our quick summarizer to turn detailed articles into easy-to-read
+          summaries. Perfect for skimming through information fast!
+        </p>
+
+        <a
+          class=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
+           focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium
+           rounded-lg text-sm sm:text-xl 2xl:text-3xl px-5 py-2.5 text-center me-2 mb-2"
+          href="https://sumz-ai-article.vercel.app/"
+          target="_blank"
+        >
+          SumzAI
+        </a>
+      </div>
+      <div className="hidden md:block">
+        <img
+          className="rounded-full max-w-full m-auto md:max-w-[75%]"
+          src="/SumzAI.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CiCalendar } from "react-icons/ci";
 import { FaRegClock } from "react-icons/fa";
+import CallToAction from "../components/CallToAction";
 
 const Home = () => {
   const [latestPost, setLatestPost] = useState(null);
@@ -20,7 +21,7 @@ const Home = () => {
           setOtherPosts(
             data.posts.slice(5).map((post) => ({
               ...post,
-              content: stripHtml(post.content).substring(0, 120) + "...",
+              content: stripHtml(post.content).substring(0, 150) + "...",
             }))
           );
         }
@@ -39,17 +40,19 @@ const Home = () => {
 
   return (
     <div className="min-h-screen px-14 py-8 flex flex-col">
-      <div className="flex flex-col lg:grid lg:grid-cols-3 gap-16 pb-6">
+      <div className="flex flex-col lg:grid lg:grid-cols-3 gap-16 mb-12">
         {latestPost && (
           <Link
             to={`/post/${latestPost.slug}`}
             className="lg:col-span-2 hover:opacity-80"
           >
             <div className="flex flex-col gap-4">
-              <h2 className="font-semibold text-4xl lg:text-6xl ">
+              <h2 className="font-semibold text-3xl lg:text-4xl 2xl:text-6xl ">
                 {latestPost.title}
               </h2>
-              <span className="text-orange-600">{latestPost.category}</span>
+              <span className="text-orange-600 2xl:text-2xl">
+                {latestPost.category}
+              </span>
               <img
                 src={latestPost.image}
                 alt="post image"
@@ -63,13 +66,13 @@ const Home = () => {
             nextToLatestPosts.map((post) => (
               <Link to={`/post/${post.slug}`}>
                 <div className="w-full border-b-[1px] pb-3 dark:border-stone-500  border-stone-300 lg:w-[75%]">
-                  <span className="text-sm text-orange-600 flex pb-1">
+                  <span className="text-sm 2xl:text-xl text-orange-600 flex pb-1">
                     {post.category}
                   </span>
-                  <h3 className="text-base 2xl:text-2xl font-semibold hover:text-orange-400 dark:hover:text-orange-300">
+                  <h3 className="text-base xl:text-2xl 2xl:text-3xl font-semibold hover:text-orange-400 dark:hover:text-orange-300">
                     {post.title}
                   </h3>
-                  <span className="pt-2 flex italic items-center justify-end gap-1 text-xs">
+                  <span className="pt-2 flex italic items-center justify-end gap-1 text-xs 2xl:text-base">
                     <FaRegClock size={12} />
                     {(post.content.length / 1000).toFixed(0) > 0
                       ? (post.content.length / 1000).toFixed(0)
@@ -81,23 +84,26 @@ const Home = () => {
             ))}
         </div>
       </div>
+      <div className="p-8 border-solid border-4 dark:border-2 border-green-400 rounded-tl-3xl rounded-br-3xl md:rounded-br-full mb-12 bg-slate-900 dark:bg-slate-950">
+        <CallToAction />
+      </div>
       <div className="flex flex-col gap-12">
         {otherPosts &&
           otherPosts.map((post) => (
             <Link to={`/post/${post.slug}`}>
               <div className="grid grid-cols-1 md:flex justify-between items-start border-t-[1px] pt-6 dark:border-stone-500 border-stone-300 hover:opacity-80">
                 <div className="md:w-[25%] mb-4 md:mb-0">
-                  <span className="text-orange-600 text-sm">
+                  <span className="text-orange-600 text-sm 2xl:text-xl">
                     {post.category}
                   </span>
-                  <h3 className="text-base 2xl:text-3xl font-semibold ">
+                  <h3 className="text-base xl:text-2xl 2xl:text-3xl font-semibold ">
                     {post.title}
                   </h3>
-                  <span className="pt-3 italic flex items-center gap-1 text-xs">
+                  <span className="pt-3 italic flex items-center gap-1 text-xs 2xl:text-sm">
                     <CiCalendar size={18} />
                     {new Date(post.createdAt).toLocaleDateString()}
                   </span>
-                  <span className="pt-3 flex italic items-center justify-start gap-1 text-xs">
+                  <span className="pt-3 flex italic items-center justify-start gap-1 text-xs 2xl:text-sm">
                     <FaRegClock size={12} />
                     {(post.content.length / 1000).toFixed(0) > 0
                       ? (post.content.length / 1000).toFixed(0)
@@ -105,7 +111,7 @@ const Home = () => {
                     min read
                   </span>
                 </div>
-                <p className="w-full md:w-[25%] md:block hidden 2xl:text-xl text-stone-500 dark:text-stone-400">
+                <p className="w-full md:w-[25%] md:block hidden xl:text-xl  text-stone-500 dark:text-stone-400">
                   {post.content}
                 </p>
                 <img
